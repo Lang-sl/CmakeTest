6ba5be593d431d95648d0f34d1a11624f1356b39
maimailang <lang.shen@udschina.com>
2023-08-08 17:13:20 +0800

    commit mfc demo

---
+++ MFCApplicationTest/MFCApplicationTest.cpp
@@ -0,0 +1,60 @@
+﻿
+// MFCApplicationTest.cpp: 定义应用程序的类行为。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MFCApplicationTest.h"
+#include "MFCApplicationTestDlg.h"
+#include "qmfcapp.h"
+#include "MainUI.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFCApplicationTestApp
+
+BEGIN_MESSAGE_MAP(CMFCApplicationTestApp, CWinApp)
+	ON_COMMAND(ID_HELP, &CWinApp::OnHelp)
+END_MESSAGE_MAP()
+
+
+// CMFCApplicationTestApp 构造
+
+CMFCApplicationTestApp::CMFCApplicationTestApp()
+{
+	// 支持重新启动管理器
+	m_dwRestartManagerSupportFlags = AFX_RESTART_MANAGER_SUPPORT_RESTART;
+
+	// TODO: 在此处添加构造代码，
+	// 将所有重要的初始化放置在 InitInstance 中
+}
+
+
+// 唯一的 CMFCApplicationTestApp 对象
+
+CMFCApplicationTestApp theApp;
+
+
+// CMFCApplicationTestApp 初始化
+
+BOOL CMFCApplicationTestApp::InitInstance()
+{
+	CWinApp::InitInstance();
+	QMfcApp::instance(this);
+	
+	MainUI ui;
+	ui.show();
+	QMfcApp::exec();
+	return FALSE;
+}
+
+int CMFCApplicationTestApp::Run()
+{
+	int result = QMfcApp::run(this);
+	delete qApp;
+	return result;
+}
+
